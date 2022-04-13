7b0ec1633562babc540d82ce0b65c05babd5df8b
iblopez93 <69406367+iblopez93@users.noreply.github.com>
2022-04-13 03:55:55 -0400

    Add logic send data from server websocket each 100 ms

---
+++ websocket/server.py
@@ -0,0 +1,24 @@
+import asyncio
+import json
+from random import randint
+
+import websockets
+
+
+async def handler(websocket):
+    while True:
+        data = {
+            'a': randint(1, 100),
+            'b': randint(0, pow(2, 32))
+        }
+        await websocket.send(json.dumps(data))
+        await asyncio.sleep(0.001)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
